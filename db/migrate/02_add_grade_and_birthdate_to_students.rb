2acb949a52c16a8d03194e1b46a9b6bcaf0528e9
victorious-messaging-4309 <wildayk@gmail.com>
2019-04-18 08:50:41 +0000

    Automatically backed up by Learn

--- db/migrate/02_add_grade_and_birthdate_to_students.rb
+++ db/migrate/02_add_grade_and_birthdate_to_students.rb
@@ -1,6 +1,7 @@
-class AddGradeAndBirthdateToStudents < ActiveRecord::Migration
+class AddGradeAndBirthdateToStudents < ActiveRecord::Migration[5.1]
 
   def change
     create_column :students, :grade, :integer
+                  :students, :birthdate, :string
   end
 end
